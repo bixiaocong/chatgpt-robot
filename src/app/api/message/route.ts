32443ef91974426afa6e57b64bacfbe0cff956fb
bicongcong <bicongcong@xiaomi.com>
2024-06-10 12:08:43 +0800

    feature: intellectual_property

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request, res: Response) {
   const { messages } = await req.json();
   const apiKey = process.env.OPENAI_API_KEY;
-  const url = "https://openai.einverne.info/v1/chat/completions";
+  const url = "https://api.openai.com/v1/chat/completions";
   const body = JSON.stringify({
     messages,
     model: "gpt-3.5-turbo",
